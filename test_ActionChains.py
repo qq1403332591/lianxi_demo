c88202f03f9fcb5014687e3d82a2f8da80b6fdaf
qq1403332591 <1403332591@qq.com>
2020-10-27 23:06:08 +0800

    selenium点击、拖动操作

--- test_ActionChains.py
+++ test_ActionChains.py
@@ -1,5 +1,5 @@
 import time
-
+import pytest
 from selenium import  webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -11,8 +11,8 @@ class TestCase:
         self.driver.get("http://sahitest.com/demo/clicks.htm")
         self.driver.implicitly_wait(3)
 
-
-    def test_click(self):
+    @pytest.mark.skip
+    def test_click(self):  # 点击事件
         el1 = self.driver.find_element(By.XPATH,"//*[@value='dbl click me']")
         el2 = self.driver.find_element(By.XPATH,"//*[@value='click me']")
         el3 = self.driver.find_element(By.XPATH,"//*[@value='right click me']")
@@ -23,6 +23,17 @@ class TestCase:
         time.sleep(3)
         actionchains.perform()
 
-    def tear_down(self):
-        self.driver.quit()
 
+
+    def test_dragdrop(self):  # 拖动释放
+        self.driver.get('http://sahitest.com/demo/dragDropMooTools.htm')
+        el1 = self.driver.find_element_by_id('dragger')
+        el2 = self.driver.find_element_by_xpath('/html/body/div[2]')
+        actionchains = ActionChains(self.driver)
+        actionchains.drag_and_drop(el1,el2)    # 然后移动到目标元素并释放鼠标按钮。
+        actionchains.perform()
+        time.sleep(3)
+
+
+    def teardown(self):
+        self.driver.quit()

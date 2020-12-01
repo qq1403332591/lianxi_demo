9d2d7076993f8f48ae9cf0c60b2208ef6774faa8
qq1403332591 <1403332591@qq.com>
2020-12-01 17:19:57 +0800

    mitmproxy的maplocal的使用

--- mitmproxy/maplocal.py
+++ mitmproxy/maplocal.py
@@ -2,7 +2,7 @@ from mitmproxy import http
 
 
 def request(flow: http.HTTPFlow) -> None:
-    if "batch" in flow.request.pretty_url and "quote.json" in flow.request.pretty_url:
+    if "stock" in flow.request.pretty_url and "quote.json" in flow.request.pretty_url:
         with open("D:\LB_HogwartsTest\mitmproxy\quote.json",encoding='utf-8') as f:
             flow.response = http.HTTPResponse.make(
                 200,  # (optional) status code

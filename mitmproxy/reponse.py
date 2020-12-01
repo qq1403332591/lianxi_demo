fa9409ad52f827517f705b9d4425d2e03f4666d0
qq1403332591 <1403332591@qq.com>
2020-12-01 17:33:42 +0800

    attribute的使用

--- mitmproxy/reponse.py
+++ mitmproxy/reponse.py
@@ -5,7 +5,7 @@ from mitmproxy import http
 
 def response(flow: http.HTTPFlow):
     # 过滤url
-    if "batch" in flow.request.pretty_url and "symbol" in flow.request.pretty_url:
+    if "quote.json" in flow.request.pretty_url and "x=" in flow.request.pretty_url:
         # 将response转化成Python对象
         data = json.loads(flow.response.content)
         # 将response中的name 修改成xxx
